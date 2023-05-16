2a43f9f1ef2c73055a629e1361fac30472641450
Joia <gustavo.joia57@gmail.com>
2023-05-16 20:26:06 -0300

    16/05/23

--- js/imprimir.js
+++ js/imprimir.js
@@ -3,18 +3,9 @@ export default function imprimir(section, nav, btn){
     btn.style.display = 'none'
     section.style.marginTop = '0'
 
-    let add = document.querySelectorAll('.btn__add')
-
-    add.forEach(b=>{
-        b.style.display = 'none'
-    })
-
     print()
     nav.style.display = 'flex'
     btn.style.display = 'block'
     section.style.marginTop = '8vh'
     
-    add.forEach(b=>{
-        b.style.display = 'block'
-    })
 }
